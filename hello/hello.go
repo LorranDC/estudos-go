436bfc886c489fe03d1011005bd33414fc879ab9
LorranDC <lorrandias150@gmail.com>
2026-04-28 22:43:40 -0300

    Return a random greeting

--- hello/hello.go
+++ hello/hello.go
@@ -6,10 +6,6 @@ import (
 				"example.com/greetings"
 )
 
-
-
-
-
 func main() {
 	//Set properties of the predefined Logger, including
 	// the log entry prefix and a flag to disable printing
@@ -18,13 +14,13 @@ func main() {
 	log.SetFlags(0)
 
 	// Request a greeting message.
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Lorran")
 	//If an error was returned, print it to the console and
 	// exit the program
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	
 	//If no error was returned, print the returned message
 	// to the console.
 	fmt.Println(message)
