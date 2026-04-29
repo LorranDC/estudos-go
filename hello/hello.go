ef4c5c40079dcf01e034154c027b88d90d4aac2a
LorranDC <lorrandias150@gmail.com>
2026-04-28 23:49:44 -0300

    Return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -13,16 +13,17 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Lorran")
-	//If an error was returned, print it to the console and
-	// exit the program
+	// A slice of names.
+	names := []string{"Lorran", "Zé", "Maria"}
+
+	//Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
 	//If no error was returned, print the returned message
 	// to the console.
-	fmt.Println(message)
+	fmt.Println(messages)
+
 }
 
