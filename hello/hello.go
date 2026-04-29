ee9f73fb651d6cc7caba6bb56ad4cd3d20537c71
LorranDC <lorrandias150@gmail.com>
2026-04-28 21:35:05 -0300

    Handling errors in GoLang

--- hello/hello.go
+++ hello/hello.go
@@ -2,7 +2,7 @@ package main
 
 import (
 				"fmt"
-
+				"log"
 				"example.com/greetings"
 )
 
@@ -11,8 +11,22 @@ import (
 
 
 func main() {
-	//Get a greeting message and print it.
-	message := greetings.Hello("Lorran")
+	//Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	//If an error was returned, print it to the console and
+	// exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
 
